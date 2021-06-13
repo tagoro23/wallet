560953696ee3a64f59c5271234a7fbad46364a07
tagoro23 <85072789+tagoro23@users.noreply.github.com>
2021-06-13 16:45:08 -0400

    Update sharedWallet.sol

--- sharedWallet.sol
+++ sharedWallet.sol
@@ -2,26 +2,28 @@
 
 pragma solidity 0.8.1;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 import "./Allowance.sol";
-contract SharedWallet is Allowance{
+
+
+
+contract sharedWallet is Allowance{
+    
     event MoneySent(address indexed _beneficiary, uint _amount);
     event MoneyReceived(address indexed _from, uint _amount);
     
-    function withdrawMoney(address payable _to, uint _amount) public onlyOwner ownerOrAllowed(_amount) {
-         require(_amount <= address(this).balance, "Contract doesn't have enough money");
-          if(!isOwner()) {
-            reduceAllowance(msg.sender, _amount);
-          }
-        emit MoneySent(_to, _amount);
+    function withdrawMoney (address payable _to, uint _amount) public ownerOrAllowed(_amount) {
+        require(_amount <= address(this).balance, "Not enough funds within the contract");
+        if(owner() != msg.sender){
+        reduceAllowance(_to, _amount);
+    }
+       emit MoneySent(_to, _amount);
         _to.transfer(_amount);
     }
     
-    function renounceOwnership() public override onlyOwner {
-        revert("can't renounceOwnership here"); //not possible with this smart contract
+    receive () external payable {
+        emit MoneyReceived(msg.sender, msg.value);
+        
     }
-
-    receive() external payable {
-         emit MoneyReceived(msg.sender, msg.value);
-
-    }
+    
 }
